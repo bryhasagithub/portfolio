bc86c36a62f5922c4c4ed84d671fd97eda463c7b
Bry Nguyen <brynguyen1331@gmail.com>
2025-05-10 20:39:36 -0700

    Updates

---
+++ precache-manifest.4e8ea5ac8a726f63e7f3cc366ff0d239.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33c66aa86e2027b058126c3d053a6d03",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "64a88db45bd85dd43981",
+    "url": "/portfolio/static/css/3.c4e31aca.chunk.css"
+  },
+  {
+    "revision": "4209dbc242c7b73bfbd8",
+    "url": "/portfolio/static/css/4.54df59d4.chunk.css"
+  },
+  {
+    "revision": "f3f15b1a505ea0036d6e",
+    "url": "/portfolio/static/css/main.fb6d1b01.chunk.css"
+  },
+  {
+    "revision": "b0941b8fa9039cd313cc",
+    "url": "/portfolio/static/js/2.37c9cdf3.chunk.js"
+  },
+  {
+    "revision": "5b571a43117d7de158db9e221eaa0225",
+    "url": "/portfolio/static/js/2.37c9cdf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64a88db45bd85dd43981",
+    "url": "/portfolio/static/js/3.be3381ef.chunk.js"
+  },
+  {
+    "revision": "4209dbc242c7b73bfbd8",
+    "url": "/portfolio/static/js/4.46247273.chunk.js"
+  },
+  {
+    "revision": "f3f15b1a505ea0036d6e",
+    "url": "/portfolio/static/js/main.d14da6d9.chunk.js"
+  },
+  {
+    "revision": "15556b9079a774464e35",
+    "url": "/portfolio/static/js/runtime-main.1b330308.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "3f2d5c135cd7946a1ed809943e31af1a",
+    "url": "/portfolio/static/media/car-model.3f2d5c13.png"
+  },
+  {
+    "revision": "cab9987e055ac0aa59a929a99c4b9c3f",
+    "url": "/portfolio/static/media/cat-chillin.cab9987e.png"
+  },
+  {
+    "revision": "7a78f795fbab0b6464d1972e6c5f728d",
+    "url": "/portfolio/static/media/couch-cad.7a78f795.png"
+  },
+  {
+    "revision": "5e5962095ad8b931e880edaa01bae37f",
+    "url": "/portfolio/static/media/doc-review.5e596209.png"
+  },
+  {
+    "revision": "f60c834acbabbd00b3814e379c9910a0",
+    "url": "/portfolio/static/media/dropin.f60c834a.png"
+  },
+  {
+    "revision": "f09496716ed707befe6f2de4fd86d325",
+    "url": "/portfolio/static/media/fabrix.f0949671.png"
+  },
+  {
+    "revision": "1cda6bd2917a6bcaae6a6af8fea115d6",
+    "url": "/portfolio/static/media/fabrix_logo.1cda6bd2.png"
+  },
+  {
+    "revision": "812ca8281ad18800324c125d35ac0af2",
+    "url": "/portfolio/static/media/gauge-cluster.812ca828.png"
+  },
+  {
+    "revision": "ecdd470e56609e1c6a7013a9d608a741",
+    "url": "/portfolio/static/media/gol.ecdd470e.png"
+  },
+  {
+    "revision": "9161a63a69ee4b7915542f869b386b88",
+    "url": "/portfolio/static/media/happy-kirby.9161a63a.gif"
+  },
+  {
+    "revision": "58fe8ad62cbfd5019881ce891a45418f",
+    "url": "/portfolio/static/media/lgec.58fe8ad6.png"
+  },
+  {
+    "revision": "bb26c7f1fabe83101e3303bb82effc68",
+    "url": "/portfolio/static/media/miata.bb26c7f1.png"
+  },
+  {
+    "revision": "75e4a6086ccaee62f7be1d61b23f8cde",
+    "url": "/portfolio/static/media/mustang.75e4a608.png"
+  },
+  {
+    "revision": "2eb8b09118e747c370ec432d24e982be",
+    "url": "/portfolio/static/media/playful_cat.2eb8b091.png"
+  },
+  {
+    "revision": "39ff0a23f3b58f2d8b6a81009fddf754",
+    "url": "/portfolio/static/media/programmer_working.39ff0a23.png"
+  },
+  {
+    "revision": "f93dd9cb806595e333b8cf1491f18d61",
+    "url": "/portfolio/static/media/retromodern.f93dd9cb.png"
+  },
+  {
+    "revision": "647f398efe6d612d9a27e36465eb3fa1",
+    "url": "/portfolio/static/media/upwork_logo.647f398e.png"
+  },
+  {
+    "revision": "d9e331029084d436b4f6b2de55033f37",
+    "url": "/portfolio/static/media/wheelwell_logo.d9e33102.jpg"
+  }
+]);
